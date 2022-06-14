87b3f05f612668c6b32b15a389c34b2321c3f739
Endang Ismaya <endang.ismaya@gmail.com>
2022-06-14 22:25:30 +0700

    register Product's model to admin panel

--- app_ecommerce/models.py
+++ app_ecommerce/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    desc = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return self.name
