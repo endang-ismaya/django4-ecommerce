d7abc262db39b75e6145688096185176d89492ee
Endang Ismaya <endang.ismaya@gmail.com>
2022-06-15 11:17:54 +0700

    add namespace to urls

--- app_ecommerce/urls.py
+++ app_ecommerce/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = "app_ecommerce"
+
 urlpatterns = [
     path("", views.index),
-    path("products", views.products),
-    path("products/<int:product_id>", views.product_detail),
+    path("products/", views.products),
+    path("products/<int:product_id>", views.product_detail, name="product_detail"),
 ]
