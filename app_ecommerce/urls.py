83cc15659d157a3ae27d51a144715036521d15b5
Endang Ismaya <endang.ismaya@gmail.com>
2022-06-15 11:00:06 +0700

    add index and detail pages

--- app_ecommerce/urls.py
+++ app_ecommerce/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
+    path("products", views.products),
+    path("products/<int:product_id>", views.product_detail),
 ]
--- app_ecommerce/views.py
+++ app_ecommerce/views.py
@@ -1,6 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World")
+
+
+def products(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "app_ecommerce/index.html", context=context)
+
+
+def product_detail(request, product_id):
+
+    try:
+        product = Product.objects.get(id=product_id)
+        context = {"product": product}
+        return render(
+            request=request, template_name="app_ecommerce/detail.html", context=context
+        )
+    except:
+        return HttpResponse("Product not found.")
