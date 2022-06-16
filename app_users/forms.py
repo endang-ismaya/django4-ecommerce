8d7abc3e7d3d2c7530aefc658c2e6aedb61ab523
Endang Ismaya <endang.ismaya@gmail.com>
2022-06-16 09:33:45 +0700

    add register page

--- app_users/forms.py
+++ app_users/forms.py
@@ -4,11 +4,55 @@ from django.contrib.auth.models import User
 
 
 class NewUserForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    username = forms.CharField(max_length=100, required=True)
-    password1 = forms.CharField(max_length=15, required=True)
-    password2 = forms.CharField(max_length=15, required=True)
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(
+            attrs={
+                "class": "focus:outline-none",
+                "style": "min-width:100%;max-width:100%",
+                "placeholder": "demo@example.com",
+            }
+        ),
+    )
+    username = forms.CharField(
+        max_length=100,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                "class": "focus:outline-none",
+                "style": "min-width:100%;max-width:100%",
+                "placeholder": "johndoe",
+            }
+        ),
+    )
+    password1 = forms.CharField(
+        max_length=15,
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "focus:outline-none",
+                "style": "min-width:100%;max-width:100%",
+            }
+        ),
+    )
+    password2 = forms.CharField(
+        max_length=15,
+        required=True,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "focus:outline-none",
+                "style": "min-width:100%;max-width:100%",
+            }
+        ),
+    )
 
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
--- app_users/views.py
+++ app_users/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import NewUserForm
 
 # Create your views here.
 def register(request):
+
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            print(user)
+
+            return redirect("/products/")
+
     form = NewUserForm()
     context = {"form": form}
 
