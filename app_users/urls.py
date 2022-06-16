77dbed8308c7a9685a59652cc5f1ce45b37dfc0d
Endang Ismaya <endang.ismaya@gmail.com>
2022-06-16 20:43:08 +0700

    add logout page

--- app_users/urls.py
+++ app_users/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_view
 
 app_name = "app_users"
 
 urlpatterns = [
-    path("register", views.register),
+    path("register/", views.register),
+    path(
+        "login/",
+        auth_view.LoginView.as_view(template_name="app_users/login.html"),
+        name="login",
+    ),
+    path(
+        "logout/",
+        auth_view.LogoutView.as_view(template_name="app_users/logout.html"),
+        name="logout",
+    ),
 ]
